79e567abe8114c416209f26fa2f671519bb28d14
Emmii Cui <emmiic@sfu.ca>
2022-03-07 02:22:00 -0800

    new commit

--- dbConfig.js
+++ dbConfig.js
@@ -1,9 +1,9 @@
-require('dotenv').config();
-
 const { Pool } = require('pg');
 
 const isProduction = process.env.NODE_ENV === "production";  // checks if our app is in production or in development
-
+if (!isProduction) {
+  require('dotenv').config();
+}
 const connectionString =`postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
 
 const pool = new Pool({
